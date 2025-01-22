96d135e7b1d27175805e26937bfda74fbef76660
Xenia <k.nikolaevna98@gmail.com>
2025-01-22 18:34:21 +0300

    fix () правки в контент

--- src/components/AboutList/AboutList.jsx
+++ src/components/AboutList/AboutList.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './AboutList.module.scss';
 
-import video1 from '../../assets/videos/about-list-1.mp4';
-import video2 from '../../assets/videos/about-list-2.mp4';
-import video3 from '../../assets/videos/about-list-3.mp4';
+import video from '../../assets/videos/about-list.mp4';
 import num1 from '../../assets/images/about-list-num-1.svg';
 import num2 from '../../assets/images/about-list-num-2.svg';
 import num3 from '../../assets/images/about-list-num-3.svg';
@@ -12,26 +10,39 @@ const items = [
   {
     title: 'Всегда в&nbsp;контакте <br>с&nbsp;заказчиком',
     desc: 'Работаем по&nbsp;системе такой&nbsp;то&nbsp;2х недельными спринтами, предоставляем результаты работ и&nbsp;внимательно слушаем наших заказчиков',
-    video: video1,
+    video: video,
     num: num1,
+    startTime: 0,
   },
   {
     title: 'Ищем лучшие решения <br>ваших задач',
     desc: 'Работаем по&nbsp;системе такой то&nbsp;2х&nbsp;недельными спринтами, предоставляем результаты работ и&nbsp;внимательно слушаем наших заказчиков',
-    video: video2,
+    video: video,
     num: num2,
+    startTime: 3,
   },
   {
     title: 'Верим <br>в&nbsp;договоренности',
     desc: 'Работаем по&nbsp;системе такой то&nbsp;2х&nbsp;недельными спринтами, предоставляем результаты работ и&nbsp;внимательно слушаем наших заказчиков',
-    video: video3,
+    video: video,
     num: num3,
+    startTime: 6,
   },
 ];
 
 function AboutList() {
   const [animateIndices, setAnimateIndices] = useState([]);
-  const refs = useRef([]);
+  const listRefs = useRef([]);
+
+  const videoRefs = useRef([]);
+  useEffect(() => {
+    videoRefs.current.forEach((videoRef, index) => {
+      if (videoRef) {
+        videoRef.currentTime = items[index].startTime;
+        videoRef.play();
+      }
+    });
+  }, []);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -47,14 +58,14 @@ function AboutList() {
       { threshold: 0.2 }
     );
 
-    refs.current.forEach((ref) => {
+    listRefs.current.forEach((ref) => {
       if (ref) {
         observer.observe(ref);
       }
     });
 
     return () => {
-      refs.current.forEach((ref) => {
+      listRefs.current.forEach((ref) => {
         if (ref) {
           observer.unobserve(ref);
         }
@@ -68,7 +79,7 @@ function AboutList() {
       <hr />
       <ul className={styles.AboutList__items}>
         {items.map((el, index) => (
-          <li className={`${styles.AboutList__item} ${animateIndices.includes(index) ? styles.animate : ''}`} key={index} data-index={index} ref={(el) => (refs.current[index] = el)}>
+          <li className={`${styles.AboutList__item} ${animateIndices.includes(index) ? styles.animate : ''}`} key={index} data-index={index} ref={(el) => (listRefs.current[index] = el)}>
             <div className={styles.AboutList__text}>
               <div className={`${styles.AboutList__header} ${animateIndices.includes(index) ? styles.animate : ''}`}>
                 <div className={styles.AboutList__num}>
@@ -79,7 +90,7 @@ function AboutList() {
               <div className={`${styles.AboutList__desc} ${animateIndices.includes(index) ? styles.animate : ''}`} dangerouslySetInnerHTML={{ __html: el.desc }} />
             </div>
             <div className={styles.AboutList__video}>
-              <video preload="auto" autoPlay loop muted>
+              <video ref={(el) => (videoRefs.current[index] = el)} preload="auto" loop muted>
                 <source src={el.video} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
