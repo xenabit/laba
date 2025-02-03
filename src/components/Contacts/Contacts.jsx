88f4372a72963bb8022d417855de455aca75e438
Xenia <k.nikolaevna98@gmail.com>
2025-02-03 16:45:52 +0300

    fix (Contacts) использование API Яндекс вместо iframe

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,17 @@
 import styles from './Contacts.module.scss';
 import map from '../../assets/images/contacts.png';
+import YandexMap from './YandexMap';
+
+// // Пример использования компонента
+const mapPoints = [
+  {
+    coords: ['55.782967', '37.580526'],
+    options: {
+      hintContent: 'Marks Group',
+      balloonContent: '3-я ул. Ямского Поля, 20, стр. 1 ',
+    },
+  },
+];
 
 export default function Contacts() {
   return (
@@ -21,30 +33,9 @@ export default function Contacts() {
           <span>Связаться</span>
         </a>
       </div>
-      <div className={styles.Contacts__map}>
-        <div style={{ position: 'relative', overflow: 'hidden' }}>
-          <a href="https://yandex.ru/maps/org/marks_group/1116551737/?utm_medium=mapframe&utm_source=maps" style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '0px' }}>
-            Marks Group
-          </a>
-          <a
-            href="https://yandex.ru/maps/213/moscow/category/architectural_firm/184107417/?utm_medium=mapframe&utm_source=maps"
-            style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '0px' }}
-          >
-            Архитектурное бюро в Москве
-          </a>
-          <a
-            href="https://yandex.ru/maps/213/moscow/category/design_institute/184107419/?utm_medium=mapframe&utm_source=maps"
-            style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '28px' }}
-          >
-            Проектная организация в Москве
-          </a>
-          <iframe
-            src="https://yandex.ru/map-widget/v1/?ll=37.789097%2C55.727793&mode=search&oid=1116551737&ol=biz&sll=37.657262%2C55.727638&source=serp_navig&text=marksgroup%20%D0%B0%D0%B4%D1%80%D0%B5%D1%81&z=11"
-            width="100%"
-            frameBorder="1"
-            allowFullScreen={true}
-            style={{ position: 'relative', height: '60vh' }}
-          ></iframe>
+      <div className={styles.Contacts__render}>
+        <div>
+          <YandexMap center={[55.753544, 37.621211]} zoom={11.5} points={mapPoints} additionalClass={styles.Contacts__map} />
         </div>
       </div>
     </section>
