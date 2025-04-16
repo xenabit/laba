67c01ece8ead41d77e3368fd56d12d9e71ea3c77
Xenia <k.nikolaevna98@gmail.com>
2025-04-16 18:53:07 +0300

    fix (CaseMarkssite) изменила слайдер с отдельными картинками на слайдер с фоном

--- src/pages/CaseMarkssite.jsx
+++ src/pages/CaseMarkssite.jsx
@@ -3,47 +3,29 @@ import '../App.scss';
 import CasesIntro from '../components/CasesIntro/CasesIntro';
 import Text from '../components/Text/Text';
 import Softwar from '../components/Softwar/Softwar';
-import CasesSlider from '../components/CasesSlider/CasesSlider';
+import CasesSliderFull from '../components/CasesSliderFull/CasesSliderFull';
 import Picture from '../components/Picture/Picture';
 import ReviewsSlider from '../components/ReviewsSlider/ReviewsSlider';
 import VideoHorizontal from '../components/VideoHorizontal/VideoHorizontal';
 import CasesItems from '../components/CasesItems/CasesItems';
 import CasesCompareSlider from '../components/CasesCompareSlider/CasesCompareSlider';
-import CaseIframe from '../components/CaseIframe/CaseIframe';
-
 import softwarLogos from '/src/constants/softwarLogos';
 
 import picture from '/src/assets/images/picture-markssite.jpg';
 import case_intro_picture from '/src/assets/images/case-intro-markssite.jpg';
-
 import video_horizontal from '/src/assets/videos/video-horizontal-markssite.mp4';
-
 import reviews_slider_author_1 from '/src/assets/images/reviews-slider-1.svg';
 import reviews_slider_author_2 from '/src/assets/images/reviews-slider-2.jpg';
-
-import case_slider_bg from '/src/assets/images/case-slider-bg-mg-1.svg';
 import case_compare_slider_bg from '/src/assets/images/case-compare-slider-bg-mg-1.svg';
-
-import case_slider_picture_left_1 from '/src/assets/images/cases-slider-markssite-left-1.png';
-import case_slider_picture_right_1 from '/src/assets/images/cases-slider-markssite-right-1.png';
-import case_slider_picture_left_2 from '/src/assets/images/cases-slider-markssite-left-2.png';
-import case_slider_picture_right_2 from '/src/assets/images/cases-slider-markssite-right-2.png';
-import case_slider_picture_left_3 from '/src/assets/images/cases-slider-markssite-left-3.png';
-import case_slider_picture_right_3 from '/src/assets/images/cases-slider-markssite-right-3.png';
-import case_slider_picture_left_4 from '/src/assets/images/cases-slider-markssite-left-4.png';
-import case_slider_picture_right_4 from '/src/assets/images/cases-slider-markssite-right-4.png';
-import case_slider_picture_left_5 from '/src/assets/images/cases-slider-markssite-left-5.png';
-import case_slider_picture_right_5 from '/src/assets/images/cases-slider-markssite-right-5.png';
-import case_slider_picture_left_6 from '/src/assets/images/cases-slider-markssite-left-6.png';
-import case_slider_picture_right_6 from '/src/assets/images/cases-slider-markssite-right-6.png';
-
-import case_slider_picture_left_1_2 from '/src/assets/images/cases-slider-markssite-left-1_2.png';
-import case_slider_picture_right_1_2 from '/src/assets/images/cases-slider-markssite-right-1_2.png';
-import case_slider_picture_left_2_2 from '/src/assets/images/cases-slider-markssite-left-2_2.png';
-import case_slider_picture_right_2_2 from '/src/assets/images/cases-slider-markssite-right-2_2.png';
-import case_slider_picture_left_3_2 from '/src/assets/images/cases-slider-markssite-left-3_2.png';
-import case_slider_picture_right_3_2 from '/src/assets/images/cases-slider-markssite-right-3_2.png';
-
+import case_slider_full_picture_1_1 from '/src/assets/images/cases-slider-full-markssite-1-1.jpg';
+import case_slider_full_picture_1_2 from '/src/assets/images/cases-slider-full-markssite-1-2.jpg';
+import case_slider_full_picture_1_3 from '/src/assets/images/cases-slider-full-markssite-1-3.jpg';
+import case_slider_full_picture_1_4 from '/src/assets/images/cases-slider-full-markssite-1-4.jpg';
+import case_slider_full_picture_1_5 from '/src/assets/images/cases-slider-full-markssite-1-5.jpg';
+import case_slider_full_picture_1_6 from '/src/assets/images/cases-slider-full-markssite-1-6.jpg';
+import case_slider_full_picture_2_1 from '/src/assets/images/cases-slider-full-markssite-2-1.jpg';
+import case_slider_full_picture_2_2 from '/src/assets/images/cases-slider-full-markssite-2-2.jpg';
+import case_slider_full_picture_2_3 from '/src/assets/images/cases-slider-full-markssite-2-3.jpg';
 import case_compare_slider_picture_left_1 from '/src/assets/images/cases-compare-slider-markssite-left-1.jpg';
 import case_compare_slider_picture_right_1 from '/src/assets/images/cases-compare-slider-markssite-right-1.jpg';
 import case_compare_slider_picture_left_2 from '/src/assets/images/cases-compare-slider-markssite-left-2.jpg';
@@ -150,71 +132,6 @@ const contentReviews = [
   },
 ];
 
-const case_slider_pictures_1 = {
-  sizes: {
-    width_left: '69.61%',
-    width_right: '79.74%',
-  },
-  bg: case_slider_bg,
-  pictures: [
-    {
-      id: '1_1',
-      picture_left: case_slider_picture_left_1,
-      picture_right: case_slider_picture_right_1,
-    },
-    {
-      id: '1_2',
-      picture_left: case_slider_picture_left_2,
-      picture_right: case_slider_picture_right_2,
-    },
-    {
-      id: '1_3',
-      picture_left: case_slider_picture_left_3,
-      picture_right: case_slider_picture_right_3,
-    },
-    {
-      id: '1_4',
-      picture_left: case_slider_picture_left_4,
-      picture_right: case_slider_picture_right_4,
-    },
-    {
-      id: '1_5',
-      picture_left: case_slider_picture_left_5,
-      picture_right: case_slider_picture_right_5,
-    },
-    {
-      id: '1_6',
-      picture_left: case_slider_picture_left_6,
-      picture_right: case_slider_picture_right_6,
-    },
-  ],
-};
-
-const case_slider_pictures_2 = {
-  sizes: {
-    width_left: '69.61%',
-    width_right: '69.61%',
-  },
-  bg: case_slider_bg,
-  pictures: [
-    {
-      id: '2_1',
-      picture_left: case_slider_picture_left_1_2,
-      picture_right: case_slider_picture_right_1_2,
-    },
-    {
-      id: '2_2',
-      picture_left: case_slider_picture_left_2_2,
-      picture_right: case_slider_picture_right_2_2,
-    },
-    {
-      id: '2_3',
-      picture_left: case_slider_picture_left_3_2,
-      picture_right: case_slider_picture_right_3_2,
-    },
-  ],
-};
-
 const case_compare_slider_pictures_1 = {
   bg: case_compare_slider_bg,
   pictures: [
@@ -231,17 +148,71 @@ const case_compare_slider_pictures_1 = {
   ],
 };
 
+const case_slider_full_content_1 = {
+  sizes: {
+    width: '100%',
+  },
+
+  pictures: [
+    {
+      id: '1_1',
+      picture: case_slider_full_picture_1_1,
+    },
+    {
+      id: '1_2',
+      picture: case_slider_full_picture_1_2,
+    },
+    {
+      id: '1_3',
+      picture: case_slider_full_picture_1_3,
+    },
+    {
+      id: '1_4',
+      picture: case_slider_full_picture_1_4,
+    },
+    {
+      id: '1_5',
+      picture: case_slider_full_picture_1_5,
+    },
+    {
+      id: '1_6',
+      picture: case_slider_full_picture_1_6,
+    },
+  ],
+};
+
+const case_slider_full_content_2 = {
+  sizes: {
+    width: '100%',
+  },
+
+  pictures: [
+    {
+      id: '2_1',
+      picture: case_slider_full_picture_2_1,
+    },
+    {
+      id: '2_2',
+      picture: case_slider_full_picture_2_2,
+    },
+    {
+      id: '2_3',
+      picture: case_slider_full_picture_2_3,
+    },
+  ],
+};
+
 export default function CaseMarkssite() {
   return (
     <main>
       <CasesIntro contentCasesIntro={contentCasesIntro} />
       <Text title={contentText[0].title} text={contentText[0].text} />
       <Softwar items={contentSoftwar} />
-      <CasesSlider items={case_slider_pictures_1} />
+      <CasesSliderFull items={case_slider_full_content_1} />
       <Text title={contentText[1].title} text={contentText[1].text} />
       <Picture src={picture} />
       <Text title={contentText[2].title} text={contentText[2].text} />
-      <CasesSlider items={case_slider_pictures_2} />
+      <CasesSliderFull items={case_slider_full_content_2} />
       <Text title={contentText[2].title} text={contentText[2].text} />
       <CasesCompareSlider items={case_compare_slider_pictures_1} />
       <VideoHorizontal videoUrl={video_horizontal} />
