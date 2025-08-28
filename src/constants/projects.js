9487d7ae64bcb6a8f9996d67755f6229c6bde763
mihlap <mihlap@mail.ru>
2025-08-28 14:07:27 +0300

    добавлены постеры, сделаны улучшения для всех видео1

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,15 +1,4 @@
-import video_1_mp4 from '../assets/videos/gallery-1.mp4';
-import video_1_webm from '../assets/videos/gallery-1.webm';
-import video_2_mp4 from '../assets/videos/gallery-2.mp4';
-import video_2_webm from '../assets/videos/gallery-2.webm';
-import video_3_mp4 from '../assets/videos/gallery-3.mp4';
-import video_3_webm from '../assets/videos/gallery-3.webm';
-import video_4_mp4 from '../assets/videos/gallery-4.mp4';
-import video_4_webm from '../assets/videos/gallery-4.webm';
-import video_5_mp4 from '../assets/videos/gallery-5.mp4';
-import video_5_webm from '../assets/videos/gallery-5.webm';
-import video_6_mp4 from '../assets/videos/gallery-6.mp4';
-import video_6_webm from '../assets/videos/gallery-6.webm';
+import { getAsset } from './assetsMap';
 
 export const projectsTypes = [
   { id: 'all', title: 'Все', type: 'all' },
@@ -25,8 +14,8 @@ export const projects = [
   {
     id: 'markssite',
     video: {
-      mp4: video_1_mp4,
-      webm: video_1_webm,
+      mp4: getAsset('gallery-1.mp4'),
+      webm: getAsset('gallery-1.webm'),
     },
     src: '/portfolio/markssite',
     title: 'marksgroup.ru',
@@ -36,8 +25,8 @@ export const projects = [
   {
     id: 'markssite-hr',
     video: {
-      mp4: video_2_mp4,
-      webm: video_2_webm,
+      mp4: getAsset('gallery-2.mp4'),
+      webm: getAsset('gallery-2.webm'),
     },
     src: '/portfolio/markssite-hr',
     title: 'HR портал',
@@ -47,8 +36,8 @@ export const projects = [
   {
     id: 'markstour',
     video: {
-      mp4: video_3_mp4,
-      webm: video_3_webm,
+      mp4: getAsset('gallery-3.mp4'),
+      webm: getAsset('gallery-3.webm'),
     },
     src: '/portfolio/markstour',
     title: 'Шоурум-тур',
@@ -58,8 +47,8 @@ export const projects = [
   {
     id: 'canon-change',
     video: {
-      mp4: video_4_mp4,
-      webm: video_4_webm,
+      mp4: getAsset('gallery-4.mp4'),
+      webm: getAsset('gallery-4.webm'),
     },
     src: '/portfolio/canon-change',
     title: 'Канон перемен',
@@ -69,8 +58,8 @@ export const projects = [
   {
     id: 'tamagotchi',
     video: {
-      mp4: video_5_mp4,
-      webm: video_5_webm,
+      mp4: getAsset('gallery-5.mp4'),
+      webm: getAsset('gallery-5.webm'),
     },
     src: '/portfolio/tamagotchi',
     title: 'Тамагочи',
@@ -80,8 +69,8 @@ export const projects = [
   {
     id: 'markscity',
     video: {
-      mp4: video_6_mp4,
-      webm: video_6_webm,
+      mp4: getAsset('gallery-6.mp4'),
+      webm: getAsset('gallery-6.webm'),
     },
     src: '/portfolio/markscity',
     title: 'MARKS-CITY',
